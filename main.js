b09afa48cd232e390f332ee5b8307a93d2078457
Siddharth <siddharthgup3@gmail.com>
2025-06-22 23:33:16 +0530

    boop

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 import { LevelSelectionScene } from './scenes/LevelSelectionScene.js';
 import { GameScene } from './scenes/GameScene.js';
+// @ts-ignore
+import { PokiPlugin } from '@poki/phaser-3'
 
 const config = {
   type: Phaser.AUTO,
@@ -24,6 +26,25 @@ const config = {
   render: {
     pixelArt: false,
     antialias: true
+  },
+  plugins: {
+    global: [
+      {
+        plugin: PokiPlugin,
+        key: 'poki',
+        start: true, // must be true, in order to load
+        data: {
+          // This must be the key/name of your loading scene
+          loadingSceneKey: 'LevelSelectionScene',
+          // This must be the key/name of your game (gameplay) scene
+          gameplaySceneKey: 'LevelSelectionScene',
+          // This will always request a commercialBreak when gameplay starts,
+          // set to false to disable this behaviour (recommended to have true,
+          // see Poki SDK docs for more details).
+          autoCommercialBreak: true
+        }
+      }
+    ]
   }
 };
 
